e87fe915c898c58acaf080ed796b0f486939bc20
Kaiky M. Ishibashi <112902634+ShibaDX@users.noreply.github.com>
2024-09-06 22:15:49 -0300

    aula 06-09

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const btnAdicionarTask = document.getElementById('addButton');
 // declara variável    /  atribui a ela o elemento com ˆ esse id
 
-function addTask() {
-    console.log("Fala guys")
-}
-
 btnAdicionarTask.setAttribute('onclick', 'addTask()')
 // pega a variável, e 1- adiciona no elemento o atributo onclick, 2- ativa a função citada
+
+function addTask() {
+    // Pegar o valor digitado para adicionar no campo de adicionar tarefa
+    const taskInput = document.getElementById("taskInput");
+
+    // Verifica se o campo está vazio
+    if (taskInput.value.length === 0) {
+        alert("Campo vazio")
+
+    } else {
+        // Se não for vazio, executa a função para adicionar a tarefa no HTML
+        adicionarLi(addTask);
+    }
+}
+function adicionarLi(addTask) {
+    const taskText = taskInput.value; // String
+    // Pegar o elemento ul do HTML
+    const taskList = document.getElementById('taskList');
+
+    // Criar o elemento li para a nossa ul
+    const newTask = document.createElement('li');
+    const taskTextNode = document.createTextNode(taskText); // Precisa converter o tipo text para Node
+    newTask.appendChild(taskTextNode);
+
+    // Adicionar o li dentro ul
+    taskList.appendChild(newTask);
+
+    // Limpa o input após adicionar a li
+    taskInput.value = "";
+}
+
+
+
+
+
